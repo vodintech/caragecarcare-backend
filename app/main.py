8183fda1296b7962322470e9bcc1b5a682166624
bahugunasiddarth <bahugunasiddhi@gmail.com>
2025-05-23 13:33:48 +0530

    update

--- app/main.py
+++ app/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:8000",
+        "https://caragecarcare.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
